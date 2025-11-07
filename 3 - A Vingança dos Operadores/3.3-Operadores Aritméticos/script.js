475330099939b935fe8a220406bae3eb9d64a000
anaeljunior <anaeljuniordigital@gmail.com>
2025-11-07 05:56:22 -0300

    Operadores aritmeticos

---
+++ 3 - A Vingança dos Operadores/3.3-Operadores Aritméticos/script.js
@@ -0,0 +1,33 @@
+/*
+
++ -> Adição
+- -> Subtração
+* -> Multiplicação
+/ -> Divisão
+% -> Resto da divisão
+++ -> Incremento
+-- -> Decremento
+** -> Exponencial
+
+*/
+
+//const result = 2 + 8 // Adição
+
+//const result = 10 - 2 // Subtração
+
+//const result = 2 * 4 // Multiplicação
+
+//const result = 16 / 2 // Divisão
+
+//const result = 10 % 3 // Resto da divisão
+
+//let result = 20 // Incremento
+//result++ 
+
+//let result = 41 // Decremento
+//result--
+
+const result = 2 ** 3 // Exponencial
+
+
+console.log(result)

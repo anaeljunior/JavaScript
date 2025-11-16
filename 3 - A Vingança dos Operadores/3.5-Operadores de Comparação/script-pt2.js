af0a3095664211681dd029cb5b906e355fad174b
anaeljunior <anaeljuniordigital@gmail.com>
2025-11-16 06:23:14 -0300

    Várias alterações

---
+++ 3 - A Vingança dos Operadores/3.5-Operadores de Comparação/script-pt2.js
@@ -0,0 +1,20 @@
+/*
+
+Operadores de Comparação
+
+ > -> Maior
+ < -> Menor
+
+ >= -> Maior ou igual
+ <= -> Menor ou igual   
+
+
+*/
+
+const firstNumber = 20
+const secondNumber = 30
+
+console.log(firstNumber > secondNumber)  // Maior
+console.log(firstNumber < secondNumber)  // Menor 
+console.log(firstNumber >= secondNumber) // Maior ou igual
+console.log(firstNumber <= secondNumber) // Menor ou igual

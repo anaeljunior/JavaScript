c0b1418a5651b85489f5fa3aaf3c7a26b44b8084
anaeljunior <anaeljuniordigital@gmail.com>
2025-11-27 13:00:46 -0300

    alt

--- 4 - As Funções Contra Atacam/4.4-For in/script.js
+++ 4 - As Funções Contra Atacam/4.4-For in/script.js
@@ -1,6 +1,9 @@
 const users = { name: 'Anael', age: 40, street: 'Rua dos Desenvolvedores' }
 
+// users.name -> Anael 
+// user['name'] -> Anael
+
 for (const key in users) {
     //console.log(key + ' : ' + users[key]) ou
-    console.log(`${key} : ${users[key]}`)
+    console.log(`${key}: ${users[key]}`)
 }

4c11016b31d17bf92642623e7a194f3526ce853b
anaeljunior <anaeljuniordigital@gmail.com>
2025-12-08 00:34:56 -0300

    Desafio resolvido

--- 4 - As Funções Contra Atacam/4.9-Desafio Resolvido/script.js
+++ 4 - As Funções Contra Atacam/4.9-Desafio Resolvido/script.js
@@ -6,3 +6,35 @@
 */
 
 const cart = [10, 244, 99, 2, 20, 33, 250]
+let finalValueWithDiscount = 0
+let finalValue = 0
+
+function calculateDiscount(price, discount) {
+    const result = (price * discount) / 100
+    /* 
+        R$658,00 // 10%
+        658 * 10 -> 6580 / 100 -> 65,80
+    */
+    return result
+}
+
+cart.forEach(value => {
+    finalValue += value
+});
+
+cart.forEach(value => {
+    if (value > 30) {
+        const discount = calculateDiscount(value, 10)
+        //finalValueWithDiscount = finalValueWithDiscount + (value - discount) --> É a mesma coisa que a linha abaixo
+        finalValueWithDiscount += (value - discount)
+    } else {
+        //finalValueWithDiscount = finalValueWithDiscount + value --> É a mesma coisa que a linha abaixo
+        finalValueWithDiscount += value
+    }
+})
+
+console.log
+(`O valor da compra foi de R$ ${finalValue.toFixed(2)}
+Você economizou R$ ${(cart.reduce((acc, val) => acc + val, 0) - finalValueWithDiscount).toFixed(2)}
+Irá pagar R$ ${finalValueWithDiscount.toFixed(2)} no total.`)
+
